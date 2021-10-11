cb24eafaef4b7b2a27597d5785a6e1ddfbd86781
Flora <mazzavillanif@gmail.com>
2021-10-11 14:29:27 +1100

    added flatList

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, SafeAreaView } from 'react-native';
+import Constants from 'expo-constants'
+
+const Item = (props) => {
+  return(
+    <View>
+      <Text>{props.text}</Text>
+    </View>
+  )
+}
 
 export default function App() {
+
+  const AppData = [
+    //objects
+    { id: "1", name: "Apple"},
+    { id: "2", name: "Orange"},
+    { id: "3", name: "Banana"},
+    { id: "4", name: "Blueberry"},
+    { id: "5", name: "Tomato"}
+  ]
+
+  const Render = ({item}) => ( <Item text={item.name} /> )
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Test app!</Text>
+      <FlatList data={AppData} keyExtractor={ (item) => item.id } renderItem={Render}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +38,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight, 
   },
 });
